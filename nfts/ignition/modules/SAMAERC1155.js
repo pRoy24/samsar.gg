4f81ac5e2c48d26d5f388e71b6db895589e51e56
proy24 <roypritam1234@gmail.com>
2024-05-05 15:27:36 +0700

    updates to contracts

--- nfts/ignition/modules/SAMAERC1155.js
+++ nfts/ignition/modules/SAMAERC1155.js
@@ -1,11 +1,16 @@
 const { buildModule } = require("@nomicfoundation/hardhat-ignition/modules");
 
+require('dotenv').config();
+
 const JAN_1ST_2030 = 1893456000;
 const ONE_GWEI = 1_000_000_000n;
 
+const DEPLOYER_WALLET_ADDRESS = process.env.DEPLOYER_WALLET_ADDRESS;
+const ADMIN_WALLET_ADDRESS = process.env.ADMIN_WALLET_ADDRESS;
+
 module.exports = buildModule("SAMERC1155Module", (m) => {
 
-  const erc1155Module = m.contract("SAMERC1155", ["0x86D7B423FBb589aeE159B481424526fdF55f795B", "0x33ee741b0FFF3C02AA392BC50F4184A44D3C710d"],
+  const erc1155Module = m.contract("SAMERC1155", [DEPLOYER_WALLET_ADDRESS, ADMIN_WALLET_ADDRESS],
     {}
   );
 
