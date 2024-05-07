e81b7d55be05c5b56c6d3ea52160eb1f26db5c4c
proy24 <roypritam1234@gmail.com>
2024-05-07 07:03:52 +0700

    updates

--- processor/src/consts/Chains.js
+++ processor/src/consts/Chains.js
@@ -1,19 +1,5 @@
 export const CHAIN_DEFINITIONS = [
   {
-    name: 'Gnosis Chiado (Testnet)',
-    key: 'gnosis_chaido',
-    id: 10200,
-    rpc: "https://rpc.chiadochain.net",
-    blockExplorer: "https://blockscout.com/gnosis/chiado"
-  },
-  {
-    name: 'Arbitrum Sepolia (Testnet)',
-    key: 'arbitrum_sepolia',
-    id: 421614,
-    rpc: "https://sepolia-rollup.arbitrum.io/rpc",
-    blockExplorer: "https://sepolia.arbiscan.io",
-  },
-  {
     name: 'Base Sepolia (Testnet)',
     key: 'base_sepolia',
     id: 84532,
